c574085cd1ceece1bbc1f1cdadea657f5db7a0b8
purerosefallen <78877@qq.com>
2017-12-08 22:16:41 +0800

    update_ai

--- ygopro-server.js
+++ ygopro-server.js
@@ -452,6 +452,8 @@
       } else if (name.slice(0, 3) === 'AI#') {
         this.hostinfo.rule = 2;
         this.hostinfo.lflist = -1;
+        this.hostinfo.time_limit = 999;
+        this.hostinfo.no_check_deck = true;
       } else if ((param = name.match(/^(\d)(\d)(T|F)(T|F)(T|F)(\d+),(\d+),(\d+)/i))) {
         this.hostinfo.rule = parseInt(param[1]);
         this.hostinfo.mode = parseInt(param[2]);
