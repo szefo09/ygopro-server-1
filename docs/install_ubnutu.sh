bd82e1ba38706aeef7a57be29a2cf873dc43b9c2
purerosefallen <78877@qq.com>
2018-04-21 21:37:36 +0800

    fix

--- docs/install_ubnutu.sh
+++ docs/install_ubnutu.sh
@@ -2,14 +2,14 @@
 
 # install script for Ubnutu 16
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
-echo y | sudo apt-get update -y
-echo y | sudo apt-get install -y curl git build-essential libreadline-dev libsqlite3-dev mono-complete
+sudo -E apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo -E tee /etc/apt/sources.list.d/mono-official.list
+echo y | sudo -E apt-get update -y
+echo y | sudo -E apt-get install -y curl git build-essential libreadline-dev libsqlite3-dev mono-complete
 
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-echo y | sudo apt-get install -y nodejs 
-sudo npm install pm2 -g
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E -E bash -
+echo y | sudo -E apt-get install -y nodejs 
+sudo -E npm install pm2 -g
 
 cd ~
 
@@ -19,14 +19,14 @@ cd lib
 wget https://nchc.dl.sourceforge.net/project/p7zip/p7zip/16.02/p7zip_16.02_src_all.tar.bz2 --no-check-certificate
 tar jxvf p7zip_16.02_src_all.tar.bz2
 cd p7zip_16.02
-sudo make all3 install
+sudo -E make all3 install
 cd ..
 
 wget http://download.redis.io/releases/redis-4.0.8.tar.gz --no-check-certificate
 tar xzfv redis-4.0.8.tar.gz
 cd redis-4.0.8
 make
-sudo make install
+sudo -E make install
 cp -rf src/redis-server ..
 cd ..
 pm2 start redis-server
@@ -34,7 +34,7 @@ pm2 start redis-server
 wget 'http://www.lua.org/ftp/lua-5.3.4.tar.gz' --no-check-certificate
 tar zxf lua-5.3.4.tar.gz
 cd lua-5.3.4
-sudo make linux test install
+sudo -E make linux test install
 cd ..
 
 wget 'http://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-beta5-src.zip?r=&ts=1457170593&use_mirror=nchc' -O premake-4.4-beta5-src.zip --no-check-certificate
@@ -42,7 +42,7 @@ wget 'http://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-b
 cd premake-4.4-beta5/build/gmake.unix/
 make
 cd ../../bin/release/
-sudo cp premake4 /usr/bin/
+sudo -E cp premake4 /usr/bin/
 cd ../../../
 
 wget 'https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz' -O libevent-2.0.22-stable.tar.gz --no-check-certificate
@@ -50,11 +50,11 @@ tar xf libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable/
 ./configure
 make
-sudo make install
-sudo ln -s /usr/local/lib/libevent-2.0.so.5 /usr/lib/libevent-2.0.so.5
-sudo ln -s /usr/local/lib/libevent-2.0.so.5 /usr/lib64/libevent-2.0.so.5
-sudo ln -s /usr/local/lib/libevent_pthreads-2.0.so.5 /usr/lib/libevent_pthreads-2.0.so.5
-sudo ln -s /usr/local/lib/libevent_pthreads-2.0.so.5 /usr/lib64/libevent_pthreads-2.0.so.5
+sudo -E make install
+sudo -E ln -s /usr/local/lib/libevent-2.0.so.5 /usr/lib/libevent-2.0.so.5
+sudo -E ln -s /usr/local/lib/libevent-2.0.so.5 /usr/lib64/libevent-2.0.so.5
+sudo -E ln -s /usr/local/lib/libevent_pthreads-2.0.so.5 /usr/lib/libevent_pthreads-2.0.so.5
+sudo -E ln -s /usr/local/lib/libevent_pthreads-2.0.so.5 /usr/lib64/libevent_pthreads-2.0.so.5
 cd ..
 
 cd ..
