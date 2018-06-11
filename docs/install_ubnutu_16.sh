35d96d1ae0cdf8618b3c30049e5ad07d96540095
purerosefallen <78877@qq.com>
2018-06-11 21:40:53 +0800

    update doc

--- docs/install_ubnutu_16.sh
+++ docs/install_ubnutu_16.sh
@@ -3,6 +3,7 @@
 # install script for Ubnutu 16
 
 sudo -E apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt-get install -y apt-transport-https
 echo "deb http://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo -E tee /etc/apt/sources.list.d/mono-official-stable.list
 echo y | sudo -E apt-get update -y
 echo y | sudo -E apt-get upgrade -y
