95675dfa80c5a9baf9c186790e1c47cad55039c4
purerosefallen <78877@qq.com>
2019-01-24 19:31:06 +0800

    fixdoc

--- docs/update.sh
+++ docs/update.sh
@@ -7,9 +7,9 @@ git reset --hard FETCH_HEAD
 cd windbot
 git fetch origin master
 git reset --hard FETCH_HEAD
-echo y | xbuild /property:Configuration=Release /property:TargetFrameworkVersion="v4.5"
+xbuild /property:Configuration=Release /property:TargetFrameworkVersion="v4.5"
 cd ../ygopro
-git pull origin server
+git fetch origin server
 git reset --hard FETCH_HEAD
 git submodule foreach git fetch origin master
 git submodule foreach git reset --hard FETCH_HEAD
