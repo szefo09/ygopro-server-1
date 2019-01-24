09ddb180e1aec950ca28d91e984257aabb67e828
purerosefallen <78877@qq.com>
2019-01-24 19:31:45 +0800

    add process count in update script

--- docs/update.sh
+++ docs/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# update script
+export PROCESS_COUNT=$(grep -c "processor" /proc/cpuinfo)
 
 git fetch origin master
 git reset --hard FETCH_HEAD
@@ -15,7 +15,7 @@ git submodule foreach git fetch origin master
 git submodule foreach git reset --hard FETCH_HEAD
 ~/premake5 gmake
 cd build
-make config=release
+make config=release -j$PROCESS_COUNT
 cd ../
 strip ygopro
 cd ../
