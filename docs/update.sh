ffec464f35e4a4b4b325e2818429b9963a6bf253
purerosefallen <78877@qq.com>
2019-01-24 19:28:21 +0800

    docs

--- docs/update.sh
+++ docs/update.sh
@@ -2,16 +2,16 @@
 
 # update script
 
-git pull origin master
+git fetch origin master
 git reset --hard FETCH_HEAD
 cd windbot
-git pull origin master
+git fetch origin master
 git reset --hard FETCH_HEAD
 echo y | xbuild /property:Configuration=Release /property:TargetFrameworkVersion="v4.5"
 cd ../ygopro
 git pull origin server
 git reset --hard FETCH_HEAD
-git submodule foreach git pull origin master
+git submodule foreach git fetch origin master
 git submodule foreach git reset --hard FETCH_HEAD
 ~/premake5 gmake
 cd build
