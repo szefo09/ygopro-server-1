519401ae8eaeb8da57d79b979072b7b63ea324b8
Szefo09 <szefo009@gmail.com>
2019-02-18 19:06:39 +0100

    updates

--- restart.js
+++ restart.js
@@ -2,14 +2,14 @@ var moment = require('moment');
 var exec = require('child_process').exec;
 var check = function() {
 	var now = moment();
-	if (now.hour()%2==0 && now.minute() == 22) {
+	if (now.hour()%3==0 && now.minute() == 22) {
 		console.log("UPDATE!");
 		exec("./update.sh");
 		setTimeout(check, 120000);		
 	}else{	
 		if (now.hour()==5&&now.minute()==40){
 			console.log("It is time NOW!");
-			exec("pm2 restart all");
+			exec("sudo pm2 restart all");
 		}
 		else {
 			console.log(now.format());
--- update.sh
+++ update.sh
@@ -7,19 +7,37 @@ git clone https://github.com/Fluorohydride/ygopro-scripts.git --recursive
 git clone https://github.com/Fluorohydride/ygopro-pre-script.git --recursive -b master
 git clone https://github.com/Ygoproco/Live2017Links.git --recursive -b master
 git clone https://github.com/szefo09/cdb.git -b master
+cd ygopro-scripts
+git reset --hard
+git pull
+cd ..
+cd ygopro-pre-script
+git reset --hard
+git pull
+cd ..
+cd Live2017Links
+git reset --hard
+git pull
+cd ..
+cd cdb
+git reset --hard
+git pull
+cd ..
+
 echo "copying"
 mkdir target
-rm -rf Live2017Links/script
+# rm -rf Live2017Links/script
 rm -rf Live2017Links/fitsetcode.cdb
 cp -u Live2017Links/*.cdb target
-mv -f cdb/*.cdb target/
-mv -f cdb/*.conf target/
+cp -f cdb/*.cdb target/
+cp -f cdb/*.conf target/
 cp -u Live2017Links/lflist.conf target
-cp -u Live2017Links/strings.conf target
+#cp -u Live2017Links/strings.conf target
 cp -u ygopro-scripts/*.lua target
 cp -u ygopro-pre-script/**/**/*.lua target
 cp -u ygopro-pre-script/**/**/**/*.lua target
-mv -f cdb/*.lua target/
+cp -f cdb/*.lua target
+rm -rf /home/pi/server/ygopro-server/updateYGOPro2/*
 cp -u target/*.cdb /home/pi/server/ygopro-server/updateYGOPro2
 cp -u target/*.conf /home/pi/server/ygopro-server/updateYGOPro2
 cp -u target/cards.cdb /home/pi/server/ygopro-server/ygopro
@@ -30,15 +48,20 @@ mkdir -p /home/pi/server/ygopro-server/ygopro/expansions/official
 mv target/official.cdb /home/pi/server/ygopro-server/ygopro/expansions/official/
 mv -f target/*.cdb /home/pi/server/ygopro-server/ygopro/expansions
 mv -f target/lflist.conf /home/pi/server/ygopro-server/ygopro
+mv -f target/strings.conf /home/pi/server/ygopro-server/ygopro
 #rm -rf Live2017Links
 #rm -rf ygopro-pre-script
 #rm -rf ygopro-scripts
 #rm -rf target
+chmod -R 777 /home/pi/server/*
 cd /home/pi/server/ygopro-server/updateYGOPro2
 git add --all
 git commit -m "autoUpdate"
 git push
+cd /home/pi/server/ygopro-server/ygopro/script
+git add --all
+git commit -m "autoUpdate"
+git push
 cd /home/pi/server/ygopro-server
-rm -rf update
-chmod -R 777 /home/pi/server/*
+#rm -rf update
 echo "done"
